7a0f14855870141194ea31ba3b091cacb4842f06
ryuuho <krjima@yahoo.co.jp>
2021-10-08 21:28:53 +0900

    TwitterAppTest

---
+++ pages/users/_id.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <p>パラメータ：{{ $route.params.id }}</p>
+    <NuxtLink to="/">topへ</NuxtLink>
+  </div>
+</template>
+
+<style>
+.container {
+  padding: 30px;
+}
+</style>
---
+++ plugins/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(
+    {
+      apiKey: "AIzaSyDk7_h6zzESf8YhKanQ7Oxo9XARrcroPmU",
+      authDomain: "twitterpj-2eaf3.firebaseapp.com",
+      projectId: "twitterpj-2eaf3",
+      storageBucket: "twitterpj-2eaf3.appspot.com",
+      messagingSenderId: "625619024575",
+      appId: "1:625619024575:web:d9f3948f0e8f04d531a40a",
+      measurementId: "G-CYL812V5S8"
+    }
+  )
+}
+  
+export default firebase
